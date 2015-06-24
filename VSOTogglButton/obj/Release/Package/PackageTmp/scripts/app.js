dd154008807a944860c3e45496ace74bdf3f40d8
Gerson Dias <gerson.dias@lambda3.com.br>
2015-06-24 17:24:48 -0300

    First functional button at workitem toolbox

--- VSOTogglButton/obj/Release/Package/PackageTmp/scripts/app.js
+++ VSOTogglButton/obj/Release/Package/PackageTmp/scripts/app.js
@@ -1,2 +1,19 @@
-﻿var el = document.getElementById('content');
-el.innerHTML('<markee>Olááá mundo</markee>');
+﻿var togglButton = (function () {
+    "use strict";
+
+    return {
+        execute: function (workItemContext) {
+            var img = $('.menu-item[title=TogglButton]').find('img');
+
+            if (img.attr('src').contains('inactive')) {
+                img.attr('src', 'https://togglbtn-vso.azurewebsites.net/images/active-16.png');
+            } else if (img.attr('src').contains('active')) {
+                img.attr('src', 'https://togglbtn-vso.azurewebsites.net/images/inactive-16.png');
+            }
+
+            alert('toggl!!!!');
+        }
+    };
+}());
+
+VSS.register('TogglButton', togglButton);
