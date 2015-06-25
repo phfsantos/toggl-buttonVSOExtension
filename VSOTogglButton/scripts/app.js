2620e8a40f9c99f59344973ce06c00a6a14f5e63
Gerson Dias <gerson.dias@lambda3.com.br>
2015-06-25 18:51:55 -0300

    Removing unecessary files and implementing toggl form

--- VSOTogglButton/scripts/app.js
+++ VSOTogglButton/scripts/app.js
@@ -1,17 +1,23 @@
-﻿var togglButton = (function () {
+﻿togglButton = (function () {
     "use strict";
 
-    return {
-        execute: function (workItemContext) {
-            var img = $('.menu-item[title=TogglButton]').find('img');
+    var changeImage = function () {
+        var img = $('.menu-item[title=TogglButton]').find('img');
 
+        if (img.length) {
             if (img.attr('src').contains('inactive')) {
                 img.attr('src', 'https://togglbtn-vso.azurewebsites.net/images/active-16.png');
             } else if (img.attr('src').contains('active')) {
                 img.attr('src', 'https://togglbtn-vso.azurewebsites.net/images/inactive-16.png');
             }
+        }
+    }
 
-            alert('toggl!!!!');
+    return {
+        execute: function (workItemContext) {
+            changeImage();
+
+            $('#container').dialog();
         }
     };
 }());
