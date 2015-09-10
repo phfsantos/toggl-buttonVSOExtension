36dc1c80110e06df7eeaa37ceb3263607c02b5b6
Gerson Dias <eu@gersondias.net>
2015-09-10 02:40:04 -0300

    Adding code for comunicate with toggl.com

--- src/VSOTogglButton/scripts/TogglButtonForm.ts
+++ src/VSOTogglButton/scripts/TogglButtonForm.ts
@@ -1,10 +1,66 @@
-﻿/// <reference path='ref/jquery.d.ts' />
+/// <reference path='ref/jquery.d.ts' />
 /// <reference path='ref/VSS.d.ts' />
 
 interface ITogglFormResponse {
     activityDescription: string;
     project: string;
     tags: string;
+}
+
+interface ITogglOpts {
+    method: string;
+    baseUrl: string;
+    token: string;
+    crendentials: any;
+    onLoad: any;
+
+}
+
+class TogglButton {
+    $ApiV8Url: string;
+    $user: any;
+
+    constructor() {
+        this.$ApiV8Url = "https://www.toggl.com/api/v8";
+
+    }
+
+    fetchUser(token: string) {
+        this.ajax('/me?with_related_data=true', {
+            token: token || ' ',
+            baseUrl: this.$ApiV8Url,
+            onLoad: function(xhr) {
+                
+                
+            }
+            
+            
+        })
+
+    }
+    
+    ajax(url: string, opts: any) {
+        var xhr = new XMLHttpRequest();
+        var method = opts.method || 'GET';
+        var baseUrl = opts.baseUrl || this.$ApiV8Url;
+        var token = opts.token || (this.$user && this.$$user.api_token);
+        var credentials = opts.credentials || null;
+
+        xhr.open(method, baseUrl, true);
+        if (opts.onLoad) {
+            xhr.addEventListener('load', function() { opts.onLoad(xhr) })
+        }
+
+        if (token && token !== ' ') {
+            xhr.setRequestHeader('Authorization', 'Basic ' + btoa(token + ':api_token'));
+        }
+
+        if (credentials) {
+            xhr.setRequestHeader('Authorization', 'Basic ' + btoa(credentials.username + ':' + credentials.password));
+        }
+        xhr.send(JSON.stringify(opts.payload));
+    }
+
 }
 
 
@@ -24,13 +80,19 @@ class TogglButtonForm {
 
         $('#txtDescription').text(this.workItem.fields["System.Title"] + " #" + this.workItem.id);
 
-        $('#projectSelect').change(function () {
+        $('#projectSelect').change(function() {
             ///TODO - Call toggl.com to receive a list of projects.
         });
 
-        $('#tagsSelect').change(function () {
+        $('#tagsSelect').change(function() {
             ///TODO - Call toggl.com to receive a list of tags.
         });
+
+        this.fetchTogglInformations();
+    };
+
+    fetchTogglInformation() {
+
     }
 
     getFormInputs() {
