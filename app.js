1fb4838a61f0b7e2c8d7a4a00fed8a34cd8deaab
Pedro Santos <ordepim@hotmail.com>
2017-01-18 22:00:44 -0500

    Small fix

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/togglButtonForm', toggl)
+app.use('/pomoTogglTimer', toggl)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
